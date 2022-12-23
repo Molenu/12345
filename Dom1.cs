a11b228829db25d30779bf89a5a53a498084167a
Dariy_Ershova <d-r-2015@mail.ru>
2022-12-24 00:16:30 +0500

    First commit

---
+++ Dom1.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет 12821 -> да 23432 -> да
+Console.WriteLine("ВВедите 5-и значное число");
+int  num=Convert.ToInt32(Console.ReadLine());
+string gth=Convert.ToString(num);
+
+    if (gth[0]==gth[4]&&gth[1]==gth[3])
+    {
+        Console.WriteLine("Yes");
+        }
+
+ else
+ {
+        Console.WriteLine("No");
+    }
